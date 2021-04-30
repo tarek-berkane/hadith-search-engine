f739c3105588e8b0598739e17159be0818425736
tarekDZ2019 <riko.jsk.320@gmail.com>
2021-04-30 20:53:09 +0100

    first commit

--- hadith_search_engine/settings.py
+++ hadith_search_engine/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*2w-v1tya6a&$gdy&j9j!a0@lb^pgupnqqm^$e8u^00r=*v$6o'
+SECRET_KEY = 'django-insecure-*!m69=midqi(_hmogy3(ylbz(&-45kayd4hd9$b(abi@5!35)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
