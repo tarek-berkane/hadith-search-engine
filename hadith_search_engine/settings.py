b9ee6ac51b8578073eab4a14dd8f17de2cc4c6f3
tarekDZ2019 <riko.jsk.320@gmail.com>
2021-05-04 12:01:02 +0100

    update urls for api

--- hadith_search_engine/settings.py
+++ hadith_search_engine/settings.py
@@ -133,3 +133,14 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/hour',
+        'user': '100/hour'
+    }
+}
